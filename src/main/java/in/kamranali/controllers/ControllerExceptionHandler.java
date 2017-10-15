13d8c7f31ef201c8610800bf5f474393e24ece3a
Kamran Ali <kamranalinitb@gmail.com>
2017-10-15 17:21:25 +0530

    Reviving Controller Advice for 400 Error

--- src/main/java/in/kamranali/controllers/ControllerExceptionHandler.java
+++ src/main/java/in/kamranali/controllers/ControllerExceptionHandler.java
@@ -1,28 +1,27 @@
 package in.kamranali.controllers;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-//import org.springframework.web.servlet.ModelAndView;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.support.WebExchangeBindException;
 
 @Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-//	@ResponseStatus(HttpStatus.BAD_REQUEST)
-//	@ExceptionHandler(NumberFormatException.class)
-//	public ModelAndView handleInvalidID(Exception exception){
-//
-//		log.error("Handling Not found Exception: ");
-//		log.error(exception.getMessage());
-//
-//		ModelAndView modelAndView = new ModelAndView();
-//		modelAndView.setViewName("400error");
-//		modelAndView.addObject("exception", exception);
-//
-//		return modelAndView;
-//	}
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(WebExchangeBindException.class)
+	public String handleInvalidID(Exception exception, Model model){
+
+		log.error("Handling Binding  Exception: ");
+		log.error(exception.getMessage());
+
+		model.addAttribute("exception", exception);
+
+		return "400error";
+	}
 }
